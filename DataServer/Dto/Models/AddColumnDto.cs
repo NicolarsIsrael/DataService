745be50287527fc1a40c426b9bfaafd01bcb1088
israel bakare <bakareisrael@gmail.com>
2022-09-05 07:45:21 +0100

    AddColumn dto structure changed

--- DataServer/Dto/Models/AddColumnDto.cs
+++ DataServer/Dto/Models/AddColumnDto.cs
@@ -2,26 +2,20 @@
 {
     public class AddColumnDto
     {
-        public AddColumnDataSet column { get; set; }
-        public string FormatColumn()
-        {
-            // column name and datatype
-            var formatedColumn = $"{column.name} {column.dataType}";
-
-            // default value
-            if (!string.IsNullOrEmpty(column.defaultValue))
-                formatedColumn += $"default \'{column.defaultValue}\'";
-
-            return formatedColumn;
-        }
-    }
-
-    public class AddColumnDataSet
-    {
         public string name { get; set; }
         public string dataType { get; set; }
         public string defaultValue { get; set; }
-        
+        public string FormatColumn()
+        {
+            // column name and datatype
+            var formatedColumn = $"{name} {dataType}";
+
+            // default value
+            if (!string.IsNullOrEmpty(defaultValue))
+                formatedColumn += $"default \'{defaultValue}\'";
+
+            return formatedColumn;
+        }
     }
 
 }
