c1e8dae7130eca53c9a218a5298d40005f712743
israel bakare <bakareisrael@gmail.com>
2022-09-06 08:34:25 +0100

    change databaseName to database and tableName to table

--- DataServer/Controllers/SqlController.cs
+++ DataServer/Controllers/SqlController.cs
@@ -21,68 +21,127 @@ namespace DataServer.Controllers
             _sqlService = sqlService;
         }
 
+        /// <summary>
+        /// The end point makes a call to the service to retrieve all items for the specified column
+        /// </summary>
+        /// <param name="database"></param>
+        /// <param name="table"></param>
+        /// <param name="columns"></param>
+        /// <returns></returns>
         [HttpGet]
         [Route("get-all")]
-        public async Task<IActionResult> GetAll(string databaseName, string tableName, string? columnNames)
+        public async Task<IActionResult> GetAll(string database, string table, string? columns)
         {
-            var result = await _sqlService.GetAll(databaseName, tableName, columnNames);
+            var result = await _sqlService.GetAll(database, table, columns);
             return Ok(result);
         }
 
+
+        /// <summary>
+        /// The end point makes a call to the service to retireve the rows that are valid for the specified key and value
+        /// </summary>
+        /// <param name="database"></param>
+        /// <param name="table"></param>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <param name="columns"></param>
+        /// <returns></returns>
         [HttpGet]
         [Route("get-by-key")]
-        public async Task<IActionResult> GetByKey(string databaseName, string tableName, string key, string value, string? columnNames)
+        public async Task<IActionResult> GetByKey(string database, string table, string key, string value, string? columns)
         {
-            var result = await _sqlService.GetByKey(databaseName, tableName, key, value, columnNames);
+            var result = await _sqlService.GetByKey(database, table, key, value, columns);
             return Ok(result);
         }
 
+        /// <summary>
+        /// The end point makes a call to the service to delete a specified row
+        /// </summary>
+        /// <param name="database"></param>
+        /// <param name="table"></param>
+        /// <param name="id"></param>
+        /// <returns></returns>
         [HttpDelete]
         [Route("delete-row")]
-        public async Task<IActionResult> DeleteRow(string databaseName, string tableName, string id)
+        public async Task<IActionResult> DeleteRow(string database, string table, string id)
         {
-            var result = await _sqlService.DeleteRow(databaseName, tableName, id);
+            var result = await _sqlService.DeleteRow(database, table, id);
             return Ok(result);
         }
 
+        /// <summary>
+        /// The end point makes a call to the service to add a row to a database table
+        /// </summary>
+        /// <param name="database"></param>
+        /// <param name="table"></param>
+        /// <param name="data"></param>
+        /// <returns></returns>
         [HttpPost]
         [Route("add-row")]
-        public async Task<IActionResult> Add(string databaseName, string tableName, [FromBody] InsertRowDto data)
+        public async Task<IActionResult> AddRow(string database, string table, [FromBody] InsertRowDto data)
         {
-            var result = await _sqlService.InsertRow(databaseName.ToLower(), tableName, data);
+            var result = await _sqlService.InsertRow(database.ToLower(), table, data);
             return Ok(result);
         }
 
+        /// <summary>
+        /// The end point makes a call to the service to update a specified row
+        /// </summary>
+        /// <param name="database"></param>
+        /// <param name="table"></param>
+        /// <param name="data"></param>
+        /// <returns></returns>
         [HttpPut]
         [Route("update-row")]
-        public async Task<IActionResult> Update(string databaseName, string tableName, [FromBody] UpdateRowDto data)
+        public async Task<IActionResult> UpdateRow(string database, string table, [FromBody] UpdateRowDto data)
         {
-            var result = await _sqlService.UpdateRow(databaseName, tableName, data);
+            var result = await _sqlService.UpdateRow(database, table, data);
             return Ok(result);
         }
 
+
+        /// <summary>
+        /// The end point makes a call to the service to create a database table
+        /// </summary>
+        /// <param name="database"></param>
+        /// <param name="table"></param>
+        /// <param name="data"></param>
+        /// <returns></returns>
         [HttpPost]
         [Route("create-table")]
-        public async Task<IActionResult> CreateTable(string databaseName, string tableName, [FromBody] CreateTableDto data)
+        public async Task<IActionResult> CreateTable(string database, string table, [FromBody] CreateTableDto data)
         {
-            var result = await (_sqlService.CreateTable(databaseName.ToLower(), tableName, data));
+            var result = await (_sqlService.CreateTable(database.ToLower(), table, data));
             return Ok(result);
         }
 
+        /// <summary>
+        /// The end point makes a call to the service to add a column to a database table
+        /// </summary>
+        /// <param name="database"></param>
+        /// <param name="table"></param>
+        /// <param name="data"></param>
+        /// <returns></returns>
         [HttpPost]
         [Route("add-column")]
-        public async Task<IActionResult> AddColumn(string databaseName, string tableName, AddColumnDto data)
+        public async Task<IActionResult> AddColumn(string database, string table, AddColumnDto data)
         {
-            var result = await (_sqlService.AddColumn(databaseName, tableName, data));
+            var result = await (_sqlService.AddColumn(database, table, data));
             return Ok(result);
         }
 
-
+        /// <summary>
+        /// The end point makes a call to the service to delete a column from a database table
+        /// </summary>
+        /// <param name="database"></param>
+        /// <param name="table"></param>
+        /// <param name="data"></param>
+        /// <returns></returns>
         [HttpDelete]
         [Route("delete-column")]
-        public async Task<IActionResult> DeleteColumn(string databaseName, string tableName, DeleteColumnDto data)
+        public async Task<IActionResult> DeleteColumn(string database, string table, DeleteColumnDto data)
         {
-            var result = await (_sqlService.DeleteColumn(databaseName, tableName, data));
+            var result = await (_sqlService.DeleteColumn(database, table, data));
             return Ok(result);
         }
     }
--- DataServer/Services/Contracts/ISqlService.cs
+++ DataServer/Services/Contracts/ISqlService.cs
@@ -10,75 +10,75 @@ namespace DataServer.Services.Contracts
         /// <summary>
         /// Retrieve all items from a table in a particular database
         /// </summary>
-        /// <param name="databaseName">Name of the database where the table can be found</param>
-        /// <param name="tableName">Name of the table where the items reside in</param>
+        /// <param name="database">Name of the database where the table can be found</param>
+        /// <param name="table">Name of the table where the items reside in</param>
         /// <param name="columnNames">Names of the columns to be retrieved. By default * to retrieve all columns</param>
         /// <returns></returns>
-        Task<BaseResponse> GetAll(string databaseName, string tableName, string columnNames = "*");
+        Task<BaseResponse> GetAll(string database, string table, string columnNames = "*");
 
 
         /// <summary>
         /// Retrieve a particular item by the specified key. The item is searched for in the specified table and database provided
         /// </summary>
-        /// <param name="databaseName">Database name where the item is expected to be found</param>
-        /// <param name="tableName">Table where the item is expected to be found</param>
+        /// <param name="database">Database name where the item is expected to be found</param>
+        /// <param name="table">Table where the item is expected to be found</param>
         /// <param name="key">Parameter to which the item can be identified by</param>
         /// <param name="columnNames">Names of the columns to be retrieved. By default * to retrieve all columns</param>
         /// <returns></returns>
-        Task<BaseResponse> GetByKey(string databaseName, string tableName, string key, string value, string columnNames = "*");
+        Task<BaseResponse> GetByKey(string database, string table, string key, string value, string columnNames = "*");
 
         /// <summary>
         /// Inserts a row into the table and database specified
         /// </summary>
-        /// <param name="databaseName">Name of the database to which the item will be placed</param>
-        /// <param name="tableName">Name of the table to which the item will be appended to</param>
+        /// <param name="database">Name of the database to which the item will be placed</param>
+        /// <param name="table">Name of the table to which the item will be appended to</param>
         /// <param name="data">model details about the item</param>
         /// <returns></returns>
-        Task<BaseResponse> InsertRow(string databaseName, string tableName, InsertRowDto data);
+        Task<BaseResponse> InsertRow(string database, string table, InsertRowDto data);
 
         /// <summary>
         /// Removes an item from the database. This item is uniquely identified by the key, table name and database
         /// </summary>
-        /// <param name="databaseName">Name of the database where the item resides</param>
-        /// <param name="tableName">Name of the table where the item resides</param>
+        /// <param name="database">Name of the database where the item resides</param>
+        /// <param name="table">Name of the table where the item resides</param>
         /// <param name="key">Name of the key to uniquely identify the item in the table</param>
         /// <returns></returns>
-        Task<BaseResponse> DeleteRow(string databaseName, string tableName, string Id);
+        Task<BaseResponse> DeleteRow(string database, string table, string Id);
 
         /// <summary>
         /// Update the details of an item in the specified table and database
         /// </summary>
-        /// <param name="databaseName">Name of the database where the item is located</param>
-        /// <param name="tableName">Name of the table where the item resides</param>
+        /// <param name="database">Name of the database where the item is located</param>
+        /// <param name="table">Name of the table where the item resides</param>
         /// <param name="data">model details about the item</param>
         /// <returns></returns>
-        Task<BaseResponse> UpdateRow(string databaseName, string tableName, UpdateRowDto data);
+        Task<BaseResponse> UpdateRow(string database, string table, UpdateRowDto data);
 
         /// <summary>
         /// Creates table in the specified database
         /// </summary>
-        /// <param name="databaseName"></param>
-        /// <param name="tableName"></param>
+        /// <param name="database"></param>
+        /// <param name="table"></param>
         /// <param name="data"></param>
         /// <returns></returns>
-        Task<BaseResponse> CreateTable(string databaseName, string tableName, CreateTableDto data);
+        Task<BaseResponse> CreateTable(string database, string table, CreateTableDto data);
 
         /// <summary>
         /// Add column to the specified database
         /// </summary>
-        /// <param name="databaseName">Name of the database where the column is to be added</param>
-        /// <param name="tableName">Name of the table to add the column</param>
+        /// <param name="database">Name of the database where the column is to be added</param>
+        /// <param name="table">Name of the table to add the column</param>
         /// <param name="data">Details of the column</param>
         /// <returns></returns>
-        Task<BaseResponse> AddColumn(string databaseName, string tableName, AddColumnDto data);
+        Task<BaseResponse> AddColumn(string database, string table, AddColumnDto data);
 
         /// <summary>
         /// Delete column from the specified database
         /// </summary>
-        /// <param name="databaseName">Name of the database where the column is to be removed from</param>
-        /// <param name="tableName">Name of the table to remove the specified column</param>
+        /// <param name="database">Name of the database where the column is to be removed from</param>
+        /// <param name="table">Name of the table to remove the specified column</param>
         /// <param name="data">Details of the column</param>
         /// <returns></returns>
-        Task<BaseResponse> DeleteColumn(string databaseName, string tableName, DeleteColumnDto data);
+        Task<BaseResponse> DeleteColumn(string database, string table, DeleteColumnDto data);
     }
 }
